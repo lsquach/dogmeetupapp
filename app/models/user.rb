405be301adf76fc7b93076dee7bdcc8fd5edbdc6
lsquach <lsquach@gmail.com>
2016-05-27 14:26:31 -0700

    completed meetup show page and added sessions helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :dogs
   has_many :meetups
+
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user.try(:authenticate, params[:password])
+  end
 end
