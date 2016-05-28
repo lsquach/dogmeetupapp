f322ee674b7a331a62b2f4899c2478a5118a2c54
lsquach <lsquach@gmail.com>
2016-05-27 18:51:57 -0700

    got meetup create and save working

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -6,6 +6,8 @@ class MeetupsController < ApplicationController
   def show
     if current_user
       @meetup = Meetup.find_by_id(params[:id])
+      user_id = @meetup.user_id
+      @organizer = User.find_by_id(user_id)
     else
       redirect_to root_path
     end
@@ -16,6 +18,18 @@ class MeetupsController < ApplicationController
       @meetup = Meetup.new
     else
       redirect_to root_path
+    end
+  end
+
+  def create
+    @meetup = Meetup.new(meetup_params)
+    @meetup.user = current_user
+    if @meetup.save
+      redirect_to @meetup
+      flash[:notice] = "Meetup saved"
+    else
+      flash[:error] = meetup.errors.full_messages.join(", ")
+      redirect_to new_user_meetup_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,41 +8,11 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
 
   resources :users, except: [:new] do
-    resources :meetups, except: [:show, :index]
+    resources :meetups, except: [:show, :index, :create]
     resources :dogs, except: [:show]
   end
 
-  resources :meetups, only: [:show, :index]
+  resources :meetups, only: [:show, :index, :create]
   resources :dogs, only: [:show]
 
 end
-
-# Prefix Verb   URI Pattern                                Controller#Action
-#             root GET    /                                          meetups#index
-#            login GET    /login(.:format)                           sessions#new
-#                  POST   /login(.:format)                           sessions#create
-#           logout GET    /logout(.:format)                          sessions#destroy
-#           signup GET    /signup(.:format)                          users#new
-#     user_meetups POST   /users/:user_id/meetups(.:format)          meetups#create
-#  new_user_meetup GET    /users/:user_id/meetups/new(.:format)      meetups#new
-# edit_user_meetup GET    /users/:user_id/meetups/:id/edit(.:format) meetups#edit
-#      user_meetup PATCH  /users/:user_id/meetups/:id(.:format)      meetups#update
-#                  PUT    /users/:user_id/meetups/:id(.:format)      meetups#update
-#                  DELETE /users/:user_id/meetups/:id(.:format)      meetups#destroy
-#        user_dogs GET    /users/:user_id/dogs(.:format)             dogs#index
-#                  POST   /users/:user_id/dogs(.:format)             dogs#create
-#     new_user_dog GET    /users/:user_id/dogs/new(.:format)         dogs#new
-#    edit_user_dog GET    /users/:user_id/dogs/:id/edit(.:format)    dogs#edit
-#         user_dog PATCH  /users/:user_id/dogs/:id(.:format)         dogs#update
-#                  PUT    /users/:user_id/dogs/:id(.:format)         dogs#update
-#                  DELETE /users/:user_id/dogs/:id(.:format)         dogs#destroy
-#            users GET    /users(.:format)                           users#index
-#                  POST   /users(.:format)                           users#create
-#        edit_user GET    /users/:id/edit(.:format)                  users#edit
-#             user GET    /users/:id(.:format)                       users#show
-#                  PATCH  /users/:id(.:format)                       users#update
-#                  PUT    /users/:id(.:format)                       users#update
-#                  DELETE /users/:id(.:format)                       users#destroy
-#          meetups GET    /meetups(.:format)                         meetups#index
-#           meetup GET    /meetups/:id(.:format)                     meetups#show
-#              dog GET    /dogs/:id(.:format)                        dogs#show
